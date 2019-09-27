54b81b293aba5ed23d62ea3cba3265f1b19c27d4
paulhong01 <paulhonghaha@gmail.com>
2019-09-26 22:30:34 -0700

    Solutions update

--- Solution34.java
+++ Solution34.java
@@ -28,16 +28,48 @@ public class Solution34 {
     }
 
     public int[] searchRange(int[] nums, int target) {
-        int[] result = {-1, -1};
+        // int[] result = {-1, -1};
 
-        int leftidx = binarysearch(nums, target, true);
+        // int leftidx = binarysearch(nums, target, true);
 
-        if (leftidx == nums.length || nums[leftidx] != target) {
-            return result;
+        // if (leftidx == nums.length || nums[leftidx] != target) {
+        //     return result;
+        // }
+
+        // result[0] = leftidx;
+        // result[1] = binarysearch(nums, target, false)-1;
+        // return result;
+
+        // Update version, TC:log(N), SC:O(1)
+        int l = 0, r = nums.length-1;
+        int left = -1, right = -1;
+        while (l <= r) {
+            int m = l + (r-l)/2;
+            if (nums[m] == target && (m == 0 || nums[m-1] != target)) {
+                left = m;
+                right = m;
+                break;
+            } else if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
         }
-
-        result[0] = leftidx;
-        result[1] = binarysearch(nums, target, false)-1;
-        return result;
+        
+        // Since we have already calculated the left part, we only need to check the right part
+        r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r-l)/2;
+            if (nums[m] == target && (m==nums.length-1 || nums[m+1] != target)) {
+                right = m;
+                break;
+            } else if (nums[m] <= target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        int[] ret = new int[] {left, right};
+        return ret;
     }
 }
