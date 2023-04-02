7dae4f71630a10df8b4b1800fbad4db6f20b40a8
Paul Hung <paulhonghaha@gmail.com>
2023-04-01 21:02:54 -0700

    Add solution2593

---
+++ Solution2593.java
@@ -0,0 +1,36 @@
+// Leetcode 2593: Find Score of an Array After Marking All Elements
+
+public class Solution2593 {
+    class Pair{
+        int num;
+        int index;
+        Pair(int num, int index)
+        { this.num = num; this.index = index; }
+    }
+
+    public long findScore(int[] nums) {
+        // TC:O(NlogN), SC:O(1)
+        int n = nums.length;
+        PriorityQueue<Pair>pq = new PriorityQueue<Pair>((Pair a, Pair b)->{
+            if (a.num != b.num) return (a.num - b.num);
+            else return (a.index - b.index);
+        });
+        for (int i = 0; i < nums.length; i++ ) pq.add(new Pair(nums[i], i));
+        boolean[] marked = new boolean[n];
+        
+        long ans = 0;
+        while(!pq.isEmpty())
+        {
+            int currNum = pq.peek().num;
+            int currIdx = pq.peek().index;
+            pq.poll();
+            if (marked[currIdx]) continue;
+            
+            ans += currNum;
+            marked[currIdx] = true;
+            if (currIdx + 1 < n) marked[currIdx + 1] = true;
+            if (currIdx - 1 >= 0) marked[currIdx - 1] = true; 
+        }
+        return ans;
+    }
+}
